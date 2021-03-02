2f21c0f53831fc918fc152777347a3cc79d75c04
ignite7 <ignite21@protonmail.com>
2021-03-03 00:36:43 +0100

    change dockerfile

--- entrypoints/setup.sh
+++ entrypoints/setup.sh
@@ -4,8 +4,5 @@ set -o errexit
 set -o nounset
 
 sudo python3 entrypoints/delete_db.py
-npm install --prefix ui/
-npm run build:dll --prefix ui/
-npm run build:dev --prefix ui/
-composer install -d api/
-docker-compose up
+docker-compose build
+docker-compose up -d
